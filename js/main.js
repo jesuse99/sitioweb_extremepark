c100fd267de4a2a34620b230cb2b8892f62e2eca
jesuse99 <jespinoza@uade.edu.ar>
2025-06-15 12:42:41 -0300

    agregue logica de comprar entradas

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const totalTicket = document.querySelector(".total-ticket");
+const buttonMinus = document.getElementById("button-minus");
+const buttonPlus = document.getElementById("button-plus");
+const cantAdulto = document.querySelector(".cant-adulto");
+const cantNino = document.querySelector(".cant-nino");
+const cantJubilado = document.querySelector(".cant-jubilado");
+
+const diaFecha = document.querySelectorAll(".cursor-pointer");
+const hoyFecha = new Date().getDate();
+
+diaFecha.forEach(element => {
+    if (element.innerText == hoyFecha) {
+        element.classList.add("bg-blue-500", "text-white", "font-bold");
+    }
+
+    element.addEventListener('click', function () {
+        diaFecha.forEach(element => {
+            element.classList.remove("bg-blue-500", "text-white", "font-bold")
+        });
+        element.classList.add("bg-blue-500", "text-white", "font-bold");
+    });
+});
+
+let cantA = 1;
+let cantN = 1;
+let cantJ = 1;
+
+let precioA = 8000;
+let precioN = 4000;
+let precioJ = 2000;
+
+function updateTotal() {
+    let total = (cantA * precioA) + (cantN * precioN) + (cantJ * precioJ)
+    totalTicket.innerText = `$ ${total}`;
+}
+
+function incA() {
+    cantA++;
+    cantAdulto.innerText = cantA;
+    updateTotal();
+}
+
+function decA() {
+    if (cantA > 0) {
+        cantA--;
+        cantAdulto.innerText = cantA;
+        updateTotal();
+    }
+}
+
+function incN() {
+    cantN++;
+    cantNino.innerText = cantN;
+    updateTotal();
+}
+
+function decN() {
+    if (cantN > 0) {
+        cantN--;
+        cantNino.innerText = cantN;
+        updateTotal();
+    }
+}
+
+function incJ() {
+    cantJ++;
+    cantJubilado.innerText = cantJ;
+    updateTotal();
+}
+
+function decJ() {
+    if (cantJ > 0) {
+        cantJ--;
+        cantJubilado.innerText = cantJ;
+        updateTotal();
+    }
+}
+
+
